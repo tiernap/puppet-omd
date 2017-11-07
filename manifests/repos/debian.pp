f5259e3f4e94441df61b4669909ae9f566576fa4
tiernap <tiernap@users.noreply.github.com>
2017-11-07 16:28:57 +0100

    fixed apt resource parameter names
    apt module has changed type of  some parameters

--- manifests/repos/debian.pp
+++ manifests/repos/debian.pp
@@ -6,10 +6,14 @@ include '::apt'
     location => 'http://labs.consol.de/repo/stable/debian',
     release => "$::lsbdistcodename",
     repos => 'main',
-    key => 'F2F97737B59ACCC92C23F8C7F8C1CA08A57B9ED7',
-    key_server => 'keys.gnupg.net',
+    key      => {
+      'id'     => 'F2F97737B59ACCC92C23F8C7F8C1CA08A57B9ED7',
+      'server' => 'keys.gnupg.net',
+    },
+    include  => {
+      'src' => false,
+    },
     before => Package['omd'],
-    include_src       => false
   }
 
 }
